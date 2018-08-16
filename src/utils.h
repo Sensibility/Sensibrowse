00aa34c76c871cc74d026d6db15e8e7a742fdd96
ocket8888 <ocket8888@gmail.com>
2018-08-16 14:07:38 -0600

    added a macro to determine if a string is numeric

--- src/utils.h
+++ src/utils.h
@@ -5,9 +5,11 @@
 
 #include "globals.h"
 
+// en_US IS THE ONLY LOCALE!
 static const std::locale LOCALE("en_US.UTF8");
-
+// USA! USA! USA! USA! USA!
 #define isspace(s) std::isspace(s, LOCALE)
+#define isnumber(s) (!s.empty() && s.find_first_not_of("0123456789") == std::string::npos)
 
 std::string urljoin(const std::string&, const std::string&);
 std::vector<std::string> split(const std::string);
